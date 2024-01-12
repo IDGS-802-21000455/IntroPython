1e664a9262d42981fe9c77d7f0d201de1f07366d
DiegoJC21 <IDGS80221000455@gmail.com>
2024-01-11 23:11:20 -0600

    DAJC-11/01/2024-TUPLAS

---
+++ tema1-Variables.py
@@ -0,0 +1,25 @@
+#Nombre de variables
+
+'''
+
+Comentarios Largos
+
+''' 
+
+numero1= "123"
+numero2="10"
+nombre="German"
+correo="german@gmail.com"
+
+print(numero1+numero2)
+
+#Conversion forzada entre tipos de datos
+
+print(int(numero1)+int(numero2))
+
+sueldo="123.203"
+valorDecimal=float(sueldo)
+print(valorDecimal)
+
+edad=110
+print(len(str(edad)))
